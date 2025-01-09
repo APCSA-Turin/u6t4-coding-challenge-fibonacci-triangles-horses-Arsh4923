2263742e453e4ef6bf84529772981af803654080
Arsh4923 <arshdeeps26@nycstudents.net>
2025-01-09 17:06:08 +0000

    LeBron

--- src/main/java/com/example/project/Point.java
+++ src/main/java/com/example/project/Point.java
@@ -3,20 +3,32 @@ package com.example.project;
 public class Point {
     private int x;
     private int y;
-    
+
     public Point(int x, int y) {
-      this.x = x;
-      this.y = y;
+        this.x = x;
+        this.y = y;
     }
-    
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     // Returns distance between this Point object and another Point object
     public double distanceTo(Point other) {
-      /* IMPLEMENT ME */
+        return Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
     }
-  
+
     // Returns a string in the format: (x, y)
     public String pointInfo() {
-      /* IMPLEMENT ME */
+        return "(" + x + ", " + y + ")";
     }
-  }
-  
+
+    public void incrementPoint(int val) {
+        x += val;
+        y += val;
+    }
+}
